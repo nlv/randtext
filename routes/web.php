e9331a17b1fa0e36cc07a3674bdd233e5e50305b
Leon Nikitin <nlv@lab321.ru>
2022-02-06 21:10:35 +0600

    Кодирование результата

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,41 @@ use RotEval\TextRandomizer\TextRandomozer;
 
 use Illuminate\Http\Request;
 
+function br2nl($str)
+{
+$str = preg_replace("/(\n|\r)/", "", $str);
+return preg_replace("/i", "\r\n", $str);
+}
+
 $router->post('/', function (Request $request) use ($router) {
 
-	$tRand = new RotEval\TextRandomizer\TextRandomizer($request->input('text'));
-    $cnt = $tRand->numVariant();
-    $res = [];
 
+// return response()->json($request->input('text'))->setEncodingOptions(JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
+
+$a = json_encode($request->input('text'),JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
+//$a = str_replace("\n","\r",$a);
+//print_r($a);
+//die();
+
+	//$tRand = new RotEval\TextRandomizer\TextRandomizer($request->input('text'));
+	$tRand = new RotEval\TextRandomizer\TextRandomizer($a);
+    $tCnt = $tRand->numVariant();
+
+    $cnt = $request->input('count');
+    if ($cnt && $cnt > 0) {
+      $cnt = min ($cnt, $tCnt);
+    } else {
+      $cnt = $tCnt;
+    }
+
+    $res = [];
 	for ($i=0; $i<$cnt; ++$i) {
-		$res[] = $tRand->getText();
+		$res[] = json_decode($tRand->getText(),JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
+//print_r($res);
+//die();
 	}
 
-    return response()->json($res);
+    return response()->json($res)->setEncodingOptions(JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
 });
 
 
